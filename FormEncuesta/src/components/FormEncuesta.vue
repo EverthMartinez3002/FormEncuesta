5e005298fb47f4505ec77036b647aecfd9bd2577
EverthMartinez3002 <00156021@uca.edu.sv>
2023-09-01 09:04:32 -0600

    'fist-commit'

---
+++ FormEncuesta/src/components/FormEncuesta.vue
@@ -0,0 +1,45 @@
+<template>
+    <form @submit.prevent="submitForm" class="container mt-4">
+        <div class="mb-3">
+            <label for="razones" class="form-label">¿Cuáles fueron las razones por las que ya no siguió asistiendo al
+                círculo?</label>
+            <textarea id="razones" v-model="razones" class="form-control" required></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="propuestas" class="form-label">¿Cuáles son sus propuestas para mejorar?</label>
+            <textarea id="propuestas" v-model="propuestas" class="form-control" required></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="actividades" class="form-label">¿Algún actividad o evento que quisiera que retomáramos o algo nuevo
+                que quisiera que propusiéramos?</label>
+            <textarea id="actividades" v-model="actividades" class="form-control" required></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            razones: '',
+            propuestas: '',
+            actividades: '',
+        };
+    },
+    methods: {
+        submitForm() {
+
+        },
+    },
+};
+</script>
+  
+<style scoped>
+form{
+}
+</style>
+  
---
+++ FormEncuesta/src/views/Encuesta.vue
@@ -0,0 +1,15 @@
+<template>
+<FormEncuesta />
+</template>
+
+<script>
+import FormEncuesta from '../components/FormEncuesta.vue'
+
+export default {
+  components: {
+    FormEncuesta
+  },
+}
+
+
+</script>
