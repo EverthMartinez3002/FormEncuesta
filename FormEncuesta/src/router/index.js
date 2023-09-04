d9a8644a473939a889627c6fa8edf92850110f35
EverthMartinez3002 <00156021@uca.edu.sv>
2023-09-04 13:58:53 -0600

    'view-home-v1'

--- FormEncuesta/src/router/index.js
+++ FormEncuesta/src/router/index.js
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Encuesta from '../views/Encuesta.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Encuesta
+      name: 'Home',
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-    }
+      path: '/encuesta',
+      name: 'Encuesta',
+      component: Encuesta,
+    },
   ]
 })
 
---
+++ FormEncuesta/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container mt-4 d-flex flex-column align-items-center">
+      <h1>Bienvenido a la Encuesta</h1>
+      <p>Por favor, haga clic en el botón para comenzar la encuesta:</p>
+      <button class="btn btn-primary" @click="toEncuesta">Comenzar Encuesta</button>
+    </div>
+  </template>
+  
+  <script>
+  import { useRouter } from 'vue-router';
+  
+  export default {
+    methods: {
+      toEncuesta() {
+        const router = useRouter();
+        router.push('/encuesta');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
