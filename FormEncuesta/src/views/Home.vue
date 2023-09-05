d81801c56f8edfb24c2faec598f2ac8948a3cdb5
EverthMartinez3002 <00156021@uca.edu.sv>
2023-09-05 11:56:53 -0600

    'home-v2'

--- FormEncuesta/src/views/Home.vue
+++ FormEncuesta/src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="container mt-4 d-flex flex-column align-items-center">
     <h1>Bienvenido a la Encuesta</h1>
-    <p>Por favor, haga clic en el botón para comenzar la encuesta:</p>
-    <button class="btn btn-primary" @click="toEncuesta">Comenzar Encuesta</button>
+    <p>Por favor, regístrese o inicie sesión para comenzar con la encuesta</p>
+
+    <!-- Opciones de Registro e Inicio de Sesión -->
+    <div class="mt-4">
+      <button class="btn btn-dark" @click="toRegistro">Registrarse</button>
+      <button class="btn btn-dark" @click="toInicioSesion" style="margin-left: 10px;">Iniciar Sesión</button>
+    </div>
   </div>
 </template>
 
@@ -13,13 +18,19 @@ export default {
   setup() {
     const router = useRouter();
 
-    const toEncuesta = () => {
-      router.push('/encuesta');
+    const toRegistro = () => {
+      router.push('/registro');
     };
 
-    return { toEncuesta };
+    const toInicioSesion = () => {
+      router.push('/iniciar-sesion');
+    };
+
+    return { toRegistro, toInicioSesion };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn btn-dark {}
+</style>
