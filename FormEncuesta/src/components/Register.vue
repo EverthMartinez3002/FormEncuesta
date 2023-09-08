feb7bf806b6dff9e40b6655463f39ba0b8640cb1
EverthMartinez3002 <00156021@uca.edu.sv>
2023-09-08 10:37:04 -0600

    'register-working'

--- FormEncuesta/src/components/Register.vue
+++ FormEncuesta/src/components/Register.vue
@@ -8,61 +8,96 @@
                 <form @submit.prevent="register">
                     <div class="mb-3">
                         <label for="username" class="form-label">Nombre de Usuario:</label>
-                        <input type="text" class="form-control" id="username" v-model="username" required />
+                        <input type="text" class="form-control" id="username" v-model="username" required
+                            :class="{ 'is-invalid': validationErrors.username }" />
+                        <div class="invalid-feedback">{{ validationErrors.username }}</div>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Contraseña:</label>
-                        <input type="password" class="form-control" id="password" v-model="password" required />
+                        <input type="password" class="form-control" id="password" v-model="password" required
+                            :class="{ 'is-invalid': validationErrors.password }" />
+                        <div class="invalid-feedback">{{ validationErrors.password }}</div>
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Correo Electrónico:</label>
-                        <input type="email" class="form-control" id="email" v-model="email" required />
+                        <input type="email" class="form-control" id="email" v-model="email" required
+                            :class="{ 'is-invalid': validationErrors.email }" />
+                        <div class="invalid-feedback">{{ validationErrors.email }}</div>
                     </div>
                     <div class="text-center">
-                        <button type="submit" class="btn btn-dark">Registrarse</button>
+                        <button type="button" @click="register" class="btn btn-dark">Registrarse</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </template>
-  
+
 <script>
-import { validateUsername, validatePassword, validateEmail } from '../components/utils/validations.js'
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+import { validateUsername, validatePassword, validateEmail } from '../components/utils/validations.js';
+
 export default {
     data() {
         return {
             username: '',
             password: '',
             email: '',
+            validationErrors: {
+                username: '',
+                password: '',
+                email: '',
+            },
         };
     },
-    register() {
-        if (!validateUsername(this.username)) {
-           
-            return;
-        }
+    methods: {
+        async register() {
+            try {
+                this.validationErrors = {};
 
-        if (!validatePassword(this.password)) {
-          
-            return;
-        }
+                if (!validateUsername(this.username)) {
+                    this.validationErrors.username = 'Nombre de usuario no válido.';
+                    return;
+                }
 
-        if (!validateEmail(this.email)) {
-         
-            return;
-        }
+                if (!validatePassword(this.password)) {
+                    this.validationErrors.password = 'Contraseña no válida.';
+                    return;
+                }
 
+                if (!validateEmail(this.email)) {
+                    this.validationErrors.email = 'Correo electrónico no válido.';
+                    return;
+                }
 
-        this.username = '';
-        this.password = '';
-        this.email = '';
+                const response = await axios.post('user/register', {
+                    username: this.username,
+                    password: this.password,
+                    email: this.email,
+                });
 
-        // this.$router.push('/dashboard');
+                if (response.status === 201) {
+                    Swal.fire({
+                        icon: 'success',
+                        text: 'Usuario registrado con éxito.',
+                    });
+
+                    setTimeout(() => {
+                        this.$router.push('/');
+                    }, 1500);
+                } else {
+                    console.error('Error al registrar el usuario:', response.data.message);
+                }
+            } catch (error) {
+                console.error('Error al registrar el usuario:', error);
+            }
+        },
     },
 };
 </script>
-  
+
 <style scoped>
 .card {
     margin-bottom: 4em;
@@ -70,5 +105,12 @@ export default {
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     background-color: #fff;
 }
+
+.is-invalid {
+    border-color: red !important;
+}
+
+.invalid-feedback {
+    color: red;
+}
 </style>
-  
--- FormEncuesta/src/main.js
+++ FormEncuesta/src/main.js
@@ -3,11 +3,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 library.add(faPaperPlane)
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = 'http://localhost:3000/api/'
 
 const app = createApp(App)
 
