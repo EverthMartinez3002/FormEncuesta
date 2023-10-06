6c26c8c76d6570e6c7688820ec24405d9f8a28c8
Everth Martinez <00156021@uca.edu.sv>
2023-10-05 21:44:52 -0600

    'config-deploy-dg'

--- FormEncuesta/src/components/utils/axios.config.js
+++ FormEncuesta/src/components/utils/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../../../store';
 
 const api = axios.create({
-  baseURL:'http://localhost:3000/api/', 
+  baseURL:'http://161.35.53.104:3000/api/', 
   headers: {
     'Content-Type': 'application/json',
   },
